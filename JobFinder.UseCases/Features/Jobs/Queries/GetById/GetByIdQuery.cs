382cf39026e12c16edcf739c06d63d4e19afecc9
Olga <olgahorozova75@gmail.com>
2026-03-03 23:39:54 +0200

    feat: Pasul 1.3 - Jobs UseCases CQRS + JobsController API

---
+++ JobFinder.UseCases/Features/Jobs/Queries/GetById/GetByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using JobFinder.Shared.DTOs.Jobs;
+using MediatR;
+
+namespace JobFinder.UseCases.Features.Jobs.Queries.GetById;
+
+public record GetJobPostingByIdQuery(int Id) : IRequest<JobPostingDto?>;
