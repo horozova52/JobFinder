e04fc0cdd00d91d73aa07286bb8177bc5942088d
Olga <olgahorozova75@gmail.com>
2026-03-21 15:55:55 +0200

    feat: improve registration implementation for candidates/employers using identity + error handling

--- JobFinder.Shared/DTOs/Identity/RegisterDto.cs
+++ JobFinder.Shared/DTOs/Identity/RegisterDto.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JobFinder.Shared.Enums;
+using System.ComponentModel.DataAnnotations;
 
-namespace JobFinder.Shared.DTOs.Identity
+namespace JobFinder.Shared.DTOs.Identity;
+
+public class RegisterDto
 {
-    internal class RegisterDto
-    {
-    }
+    [Required(ErrorMessage = "Email-ul este obligatoriu")]
+    [EmailAddress(ErrorMessage = "Email invalid")]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Parola este obligatorie")]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "Parola trebuie să aibă între 6 și 100 caractere")]
+    public string Password { get; set; } = null!;
+
+    [Required(ErrorMessage = "Confirmare parolă obligatorie")]
+    [Compare("Password", ErrorMessage = "Parolele nu coincid")]
+    public string ConfirmPassword { get; set; } = null!;
+
+    [Required(ErrorMessage = "Numele este obligatoriu")]
+    [StringLength(200)]
+    public string FullName { get; set; } = null!;
+
+    [Required]
+    public UserType UserType { get; set; }
 }
